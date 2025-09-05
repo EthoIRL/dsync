5b66edc71ae2dd75bb46d7c6b7e5647fcfd2ec40
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-09-05 12:33:41 -0700

    Handle ctrlc exiting in server

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,3 +1,6 @@
+use std::sync::Arc;
+use std::sync::atomic::{AtomicBool, Ordering};
+
 mod network;
 
 pub mod proto {
@@ -10,5 +13,16 @@ pub mod proto {
 }
 
 fn main() {
-    println!("Hello, world!");
+    println!("[*] [DSYNC] - [0.1.0] - [ETHO] [*]");
+
+    let application_running = Arc::new(AtomicBool::new(true));
+    let running_clone = Arc::clone(&application_running);
+
+    ctrlc::set_handler(move || {
+        running_clone.store(false, Ordering::SeqCst);
+        println!("[*] [DSYNC] Server shutting down gracefully...");
+    }).expect("[*] [DSYNC] Error setting Ctrl-C handler");
+
+    while application_running.load(Ordering::SeqCst) {
+    }
 }
