69fe93a6b0f2fafbf7e1d7f98fd75e0b3dae91f2
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-09-07 04:11:13 -0700

    Update object_sync.rs

--- client/src/network/handlers/object_sync.rs
+++ client/src/network/handlers/object_sync.rs
@@ -42,6 +42,9 @@ impl GenericHandler for ObjectSync {
         let object_hash = hash_object(&path)?;
         let chunk_hashes: Vec<u64> = hash_file_chunks(&path)?;
 
+        // TODO: To handle sync response more appropriately, there should be a enum of the file state.
+        // E.g. Fine, Deleted
+
         let sync_response = SyncResponse {
             object_id: sync.object_id,
             object_hash,
