d6151e676d20b9a434b686eee2abfefbf749d000
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-09-13 22:57:21 -0700

    Ignore child items and only show directories

--- client/src/network/handlers/object_list_response.rs
+++ client/src/network/handlers/object_list_response.rs
@@ -6,7 +6,6 @@ use redb::{Database, ReadableDatabase};
 use std::error::Error;
 use std::net::TcpStream;
 use std::path::PathBuf;
-use std::ptr::read;
 use std::sync::Arc;
 use crate::network::tools::prototools;
 
@@ -25,27 +24,20 @@ impl GenericHandler for ObjectListResponse {
                 .zip(list_response.path.iter()
                 .zip(list_response.parent_directory.iter()))
         {
+            if *is_child {
+                continue;
+            }
+
             let object_id = prototools::parse_object_id(&object_id)?;
 
             match object_table.get(&object_id)? {
                 None => {
-                    println!("[*] [DSYNC] Object not synced locally (Path: {})", path);
-
-                    // let write_txn = database.begin_write()?;
-                    // {
-                    //     let mut objects = write_txn.open_table(OBJECTS_LOCAL_TABLE)?;
-                    //     objects.remove(&object_id)?;
-                    // }
-                    // write_txn.commit()?;
+                    println!("[*] [DSYNC] Not Synced: [{}] [{}]", path, prototools::object_id_hex(&object_id));
                 },
                 Some(object) => {
-                    let hex_object_id: String = object_id.iter()
-                        .map(|byte| format!("{:02X}", byte))
-                        .collect();
-
                     if let Some(file_name) = PathBuf::from(path).file_name() {
                         if let Some(file_name) = file_name.to_str() {
-                            println!("[*] [DSYNC] [ListResponse] [{}]<->[{}] [ID: {}]", file_name, object.value(), hex_object_id);
+                            println!("[*] [DSYNC] [ListResponse] [{}]<->[{}] [ID: {}]", file_name, object.value(), prototools::object_id_hex(&object_id));
                         }
                     }
                 }
