5691eddf0a086a9158303121ddd4ebbf6673d427
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-09-20 18:11:06 -0700

    Dsync database related rather than filesystem
    If the user deletes the direcrory than dsync's for whatever reason, it would break.
    
    This searches the entire db for related children.

--- client/src/commands/local.rs
+++ client/src/commands/local.rs
@@ -1,9 +1,7 @@
 use crate::cli;
 use crate::tables::OBJECTS_LOCAL_TABLE;
-use redb::{Database, ReadableTable};
+use redb::{Database, ReadableTable, Table};
 use std::error::Error;
-use std::fs;
-use std::path::PathBuf;
 
 pub fn handle_dsync(database: &Database, target: &String) -> Result<(), Box<dyn Error>>{
     let id = cli::get_id_from_target(database, target)?;
@@ -17,29 +15,9 @@ pub fn handle_dsync(database: &Database, target: &String) -> Result<(), Box<dyn 
             None => return Err("Couldn't find object".into())
         };
 
-        let object_path = PathBuf::from(object_path);
-
-        if object_path.is_dir() {
-            let paths_to_remove = dsync_recursion_traversal(&object_path);
-
-            if !paths_to_remove.is_empty() {
-                let mut ids: Vec<[u8; 4]> = Vec::new();
-
-                for object in object_table.iter()? {
-                    let object = object?;
-
-                    let object_id = object.0.value();
-                    let object_path = object.1.value();
-
-                    if paths_to_remove.contains(&object_path) {
-                        ids.push(object_id);
-                    }
-                }
-
-                for id in ids {
-                    object_table.remove(&id)?;
-                }
-            }
+        let related_objects = dsync_find_related(&object_path, &object_table)?;
+        for related_object_id in related_objects {
+            object_table.remove(&related_object_id)?;
         }
 
         object_table.remove(&id)?;
@@ -49,21 +27,19 @@ pub fn handle_dsync(database: &Database, target: &String) -> Result<(), Box<dyn 
     Ok(())
 }
 
+fn dsync_find_related(parent_path: &String, table: &Table<[u8; 4], String>) -> Result<Vec<[u8; 4]>, Box<dyn Error>> {
+    let mut related_object_ids: Vec<[u8; 4]> = Vec::new();
 
-fn dsync_recursion_traversal(directory: &PathBuf) -> Vec<String> {
-    let mut paths_to_remove: Vec<String> = Vec::new();
+    for object in table.iter()? {
+        if let Ok(object) = object {
+            let object_id = object.0.value();
+            let object_path = object.1.value();
 
-    fs::read_dir(&directory).unwrap()
-        .for_each(|entry| {
-            if let Ok(entry) = entry {
-                println!("Entry: {}", entry.path().display());
-                if entry.path().is_dir() {
-                    paths_to_remove.append(&mut dsync_recursion_traversal(&entry.path()));
-                }
-
-                paths_to_remove.push(entry.path().to_str().unwrap().to_string());
+            if object_path.contains(parent_path) {
+                related_object_ids.push(object_id);
             }
-        });
+        }
+    }
 
-    paths_to_remove
+    Ok(related_object_ids)
 }
