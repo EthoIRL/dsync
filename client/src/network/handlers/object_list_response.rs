4801f140c3d0c9ef062c7863d5f61b2e112e6819
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-09-26 11:50:02 -0700

    Display directories better when requesting list

--- client/src/network/handlers/object_list_response.rs
+++ client/src/network/handlers/object_list_response.rs
@@ -1,13 +1,14 @@
 use crate::config::Config;
 use crate::network::packet::{GenericHandler, GenericPacket};
+use crate::network::tools::prototools;
 use crate::proto::comms::ListResponse;
 use crate::tables::OBJECTS_LOCAL_TABLE;
 use redb::{Database, ReadableDatabase};
 use std::error::Error;
+use std::fs;
 use std::net::TcpStream;
 use std::path::PathBuf;
 use std::sync::Arc;
-use crate::network::tools::prototools;
 
 pub struct ObjectListResponse;
 
@@ -18,32 +19,99 @@ impl GenericHandler for ObjectListResponse {
         let read_txn = database.begin_read()?;
         let object_table = read_txn.open_table(OBJECTS_LOCAL_TABLE)?;
 
-        for ((object_id, is_child), (path, parent_directory)) in
-            list_response.object_id.iter()
-                .zip(list_response.is_child.iter())
-                .zip(list_response.path.iter()
-                .zip(list_response.parent_directory.iter()))
-        {
-            if *is_child {
-                continue;
+        for index in 0..list_response.object_id.len() {
+            let object_is_child = &list_response.is_child[index];
+
+            if *object_is_child {
+                continue
             }
 
-            let object_id = prototools::parse_object_id(&object_id)?;
+            let object_id = prototools::parse_object_id(&list_response.object_id[index])?;
 
-            match object_table.get(&object_id)? {
-                None => {
-                    println!("[*] [DSYNC] Not Synced: [{}] [{}]", path, prototools::object_id_hex(&object_id));
-                },
-                Some(object) => {
-                    if let Some(file_name) = PathBuf::from(path).file_name() {
-                        if let Some(file_name) = file_name.to_str() {
-                            println!("[*] [DSYNC] [ListResponse] [{}]<->[{}] [ID: {}]", file_name, object.value(), prototools::object_id_hex(&object_id));
-                        }
-                    }
+            if let Some(object) = object_table.get(&object_id)? {
+                let path = PathBuf::from(object.value());
+
+                println!("[{}] => [{}]", prototools::object_id_hex(&object_id), object.value());
+
+                if path.is_dir() {
+                    recursive_tui(&path, &list_response, 0);
+                    println!("<-------------------------------------->");
+                    continue;
                 }
             }
         }
+        println!();
 
         Ok(())
+    }
+}
+
+fn recursive_tui(tld_directory: &PathBuf, list_response: &ListResponse, depth: usize) {
+    let entries = match fs::read_dir(tld_directory) {
+        Ok(e) => e.filter_map(Result::ok).collect::<Vec<_>>(),
+        Err(e) => {
+            eprintln!("[*] [DSYNC] Error reading directory {}: {}", tld_directory.display(), e);
+            return;
+        }
+    };
+
+    let mut dirs = Vec::new();
+    let mut files = Vec::new();
+
+    for entry in entries {
+        let path = entry.path();
+        if path.is_dir() {
+            dirs.push(entry);
+        } else {
+            files.push(entry);
+        }
+    }
+
+    dirs.sort_by_key(|e| e.file_name());
+    files.sort_by_key(|e| e.file_name());
+
+    for dir in dirs {
+        process_entry(&dir, list_response, depth);
+    }
+
+    for file in files {
+        process_entry(&file, list_response, depth);
+    }
+}
+
+fn process_entry(entry: &fs::DirEntry, list_response: &ListResponse, depth: usize) {
+    let path = entry.path();
+    let file_name = match path.file_name().and_then(|f| f.to_str()) {
+        Some(name) => name,
+        None => return,
+    };
+
+    let mut is_synced = false;
+
+    for (i, remote_name) in list_response.name.iter().enumerate() {
+        if file_name.contains(remote_name) {
+            let object_id = match prototools::parse_object_id(&list_response.object_id[i]) {
+                Ok(id) => id,
+                Err(_) => continue,
+            };
+
+            println!(
+                "|  {}[{}] => [{}]",
+                "    ".repeat(depth),
+                prototools::object_id_hex(&object_id),
+                file_name
+            );
+
+            is_synced = true;
+            break;
+        }
+    }
+
+    if is_synced {
+        if path.is_dir() {
+            recursive_tui(&path, list_response, depth + 1);
+        }
+    } else {
+        println!("|  {}[????????] => [{}]", "    ".repeat(depth), file_name);
     }
 }
--- server/src/network/handlers/list.rs
+++ server/src/network/handlers/list.rs
@@ -4,6 +4,7 @@ use crate::network::packet::{GenericHandler, GenericPacket};
 use crate::tables::OBJECTS_TABLE;
 use redb::{Database, ReadableDatabase, ReadableTable};
 use std::net::TcpStream;
+use std::path::PathBuf;
 use std::sync::Arc;
 use crate::network::packet;
 use crate::proto::comms::ListResponse;
@@ -16,30 +17,50 @@ impl GenericHandler for List {
         let read_txn = database.begin_read()?;
         let object_table = read_txn.open_table(OBJECTS_TABLE)?;
         
-        println!("[*] [DSYNC] [List] Client Request");
+        println!("[*] [DSYNC] [List] Client requested all objects");
 
         let mut object_ids: Vec<Vec<u8>> = Vec::new();
-        let mut paths: Vec<String> = Vec::new();
+        let mut is_directories: Vec<bool> = Vec::new();
         let mut is_childs: Vec<bool> = Vec::new();
-        let mut parent_directorys: Vec<String> = Vec::new();
+        let mut names: Vec<String> = Vec::new();
 
         for object_kv in object_table.iter()? {
             if let Ok(object_kv) = object_kv {
                 let object_id = object_kv.0.value();
                 let object: Object = bitcode::decode(&*object_kv.1.value())?;
 
+                println!("{} {} {} {:#?} {} {:#?}", object.path, object.is_directory, object.hostname, object.parent_tree, object.child_of_tree, object.children_ids);
+
                 object_ids.push(object_id.to_vec());
-                paths.push(object.path);
+                is_directories.push(object.is_directory);
+
+                let name = match object.parent_tree {
+                    None => {
+                        PathBuf::from(object.path).file_name().unwrap().to_str().unwrap().to_string()
+                    },
+                    Some(parent_tree) => {
+                        let object_path = PathBuf::from(&object.path);
+                        let parent_path = PathBuf::from(parent_tree);
+
+                        match object_path.strip_prefix(parent_path) {
+                            Err(_) => continue,
+                            Ok(name_path) => match name_path.to_str() {
+                                None => continue,
+                                Some(name_path) => name_path.to_string(),
+                            }
+                        }
+                    }
+                };
+                names.push(name);
                 is_childs.push(object.child_of_tree);
-                parent_directorys.push(object.parent_tree.unwrap_or_else(String::new))
             }
         }
 
         let response = ListResponse {
             object_id: object_ids,
-            path: paths,
+            is_directory: is_directories,
             is_child: is_childs,
-            parent_directory: parent_directorys
+            name: names,
         };
 
         packet::send_packet(stream, &mut [PacketKind::ListResponse as u8], response)?;
