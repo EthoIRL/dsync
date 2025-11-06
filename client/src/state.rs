fb92f045d8425482755264181c38103560cad2d4
Etho <58352929+EthoIRL@users.noreply.github.com>
2025-11-05 16:45:56 -0800

    Expose AES cipher with shared secret

--- client/src/state.rs
+++ client/src/state.rs
@@ -1,16 +1,34 @@
-use std::sync::atomic::AtomicBool;
 use crate::config::Config;
+use aes::cipher::KeyInit;
+use aes::Aes256;
+use sha2::{Digest, Sha256};
+use std::sync::atomic::AtomicBool;
 
 pub struct ClientState {
     pub running: AtomicBool,
-    pub config: Config
+    pub config: Config,
+
+    pub aes_cipher: Option<Aes256>,
 }
 
 impl ClientState {
     pub fn new(config: Config) -> ClientState {
+        let aes_cipher = match &config.shared_secret {
+            None => None,
+            Some(secret) => {
+                let mut sha_hasher = Sha256::new();
+                sha_hasher.update(secret.as_bytes());
+
+                let secret_hash = sha_hasher.finalize();
+
+                Some(Aes256::new(&secret_hash))
+            }
+        };
+
         ClientState {
             running: AtomicBool::new(true),
-            config
+            config,
+            aes_cipher
         }
     }
 }
